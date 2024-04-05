5941770682dfa884553255b86cf2c5238eb4fe7c
onlinesvxonline <153940673+onlinesvxonline@users.noreply.github.com>
2024-04-05 17:23:39 +0300

    Add files via upload

---
+++ HomeWork1/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace HomeWork1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length == 3)
+            {
+                int result = 0;
+                int num1 = int.Parse(args[0]);
+                string operation = args[1];
+                int num2 = int.Parse(args[2]);
+                if (operation == "+")
+                {
+                    result = num1 + num2;
+                    Console.WriteLine($"{num1} + {num2} = {result}");
+                }
+                if (operation == "-")
+                {
+                    result = num1 - num2;
+                    Console.WriteLine($"{num1} - {num2} = {result}");
+                }
+                if (operation == "*")
+                {
+                    result = num1 * num2;
+                    Console.WriteLine($"{num1} * {num2} = {result}");
+                }
+                if (operation == "/")
+                {
+                    result = num1 / num2;
+                    Console.WriteLine($"{num1} / {num2} = {result}");
+                }
+            }
+        }
+    }
+}
